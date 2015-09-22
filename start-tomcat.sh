f97a3c39f1265528843f3926a6f59a597b0ed552
Thomas Krille <t.krille@tarent.de>
2015-09-22 09:37:47 +0200

    Move JVM options to the right location
    Currently we put our custom JVM options into the `/etc/default/tomcat7`.
    That's obviously wrong, because we don't start Tomcat via SysVinit, but
    directly with `catalina.sh`. So this commits adds the respective
    environment variable to the call to `catalina.sh` and removes the change
    of `JAVA_OPTS` in `/etc/default/tomcat7`.

--- start-tomcat.sh
+++ start-tomcat.sh
@@ -8,5 +8,5 @@ fi
 exec sudo -u tomcat7 -g tomcat7 \
     CATALINA_BASE=/var/lib/tomcat7 \
     CATALINA_TMPDIR=/tmp/tomcat7-tomcat7-tmp \
+    CATALINA_OPTS="-Djava.awt.headless=true -Xms512m -Xmx2048m -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1024m" \
     /usr/share/tomcat7/bin/catalina.sh $DEBUG_COMMAND run
-
